e9cbe0c4de12fe428e48506e0bddfe876ba04779
gotonadeem <gotoabnadeem@gmail.com>
2019-12-24 18:51:58 +0530

    first change

---
+++ routes/users.js
@@ -0,0 +1,11 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('nadeem user');
+});
+router.get('/xyz', function(req, res, next) {
+  res.send('xyz user');
+});
+module.exports = router;
